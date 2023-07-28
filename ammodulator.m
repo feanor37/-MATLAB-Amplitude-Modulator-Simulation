deb60f78eba6616a5676ab8644b5ff8550011eef
Noah <nuhkaangn@gmail.com>
2023-07-28 16:24:24 +0300

    **MATLAB-Amplitude-Modulator-Simulation

---
+++ ammodulator.m
@@ -0,0 +1,24 @@
+
+Fs = 1000; 
+t = 0:0.5/Fs:1-1/Fs;
+-----------------------------------------------------
+
+
+Lütfen Kodların Tamamı için İletişime Geçin
+
+https://www.linkedin.com/in/nuh-kaan-gun-59172b248/
+
+  ----------------------------------------------------
+
+subplot(3,1,3);
+plot(t, modulated_signal);
+xlabel('Zaman (s)');
+ylabel('Modülasyonlu Sinyal');
+title('Modülasyonlu Sinyal');
+
+% Grafiği gösterme
+figure;
+plot(t, modulated_signal);
+xlabel('Zaman (s)');
+ylabel('Modülasyonlu Sinyal');
+title('Modülasyonlu Sinyal');
